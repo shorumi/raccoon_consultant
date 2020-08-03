5c2be07643ec7f3fbc6b2b3001caaf44e76c42fd
Thiago <thiagofelipepecanha@gmail.com>
2020-08-02 23:51:41 -0300

    First Commit

---
+++ app/src/test/java/br/com/dasa/labexam/LabExamApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dasa.labexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
