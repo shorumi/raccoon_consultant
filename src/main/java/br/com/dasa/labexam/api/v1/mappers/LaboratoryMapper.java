df96114b6d9a2f3197b34161ca0afbec3d78a0f8
Thiago <thiagofelipepecanha@gmail.com>
2020-08-05 23:06:18 -0300

    Add mapping annotation to the laboratory mapper

--- src/main/java/br/com/dasa/labexam/api/v1/mappers/LaboratoryMapper.java
+++ src/main/java/br/com/dasa/labexam/api/v1/mappers/LaboratoryMapper.java
@@ -3,11 +3,13 @@ package br.com.dasa.labexam.api.v1.mappers;
 import br.com.dasa.labexam.api.v1.models.LaboratoryDTO;
 import br.com.dasa.labexam.entities.Laboratory;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface LaboratoryMapper {
   LaboratoryMapper INSTANCE = Mappers.getMapper(LaboratoryMapper.class);
 
+  @Mapping(source = "id", target = "id")
   LaboratoryDTO laboratoryToLaboratoryDTO(Laboratory laboratory);
 }
