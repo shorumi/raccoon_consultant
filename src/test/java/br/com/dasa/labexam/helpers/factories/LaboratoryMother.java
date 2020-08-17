1d577bdd8f174070b36c905f011759626fc98957
Thiago <thiagofelipepecanha@gmail.com>
2020-08-17 13:48:16 -0300

    Improves Factory Mother Lab
    - Add an ID random generated

--- src/test/java/br/com/dasa/labexam/helpers/factories/LaboratoryMother.java
+++ src/test/java/br/com/dasa/labexam/helpers/factories/LaboratoryMother.java
@@ -3,10 +3,14 @@ package br.com.dasa.labexam.helpers.factories;
 import br.com.dasa.labexam.entities.Laboratory;
 import br.com.dasa.labexam.entities.Status;
 
+import java.util.Random;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+
 public class LaboratoryMother {
   public static Laboratory.LaboratoryBuilder complete() {
     return Laboratory.builder()
-            .id(1L)
+            .id(new Random().nextLong())
             .name("Mr. Pickles")
             .address("Veggies street")
             .status(Status.ACTIVE)
