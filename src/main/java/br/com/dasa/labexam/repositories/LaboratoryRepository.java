5d9f3fe17925d797ccbd0dbd95cc7127ad19479d
Thiago <thiagofelipepecanha@gmail.com>
2020-08-17 13:12:51 -0300

    Create a find all labs by status method in repo

--- src/main/java/br/com/dasa/labexam/repositories/LaboratoryRepository.java
+++ src/main/java/br/com/dasa/labexam/repositories/LaboratoryRepository.java
@@ -1,7 +1,14 @@
 package br.com.dasa.labexam.repositories;
 
 import br.com.dasa.labexam.entities.Laboratory;
+import br.com.dasa.labexam.entities.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LaboratoryRepository extends JpaRepository<Laboratory, Long> {
+  @Query("SELECT l FROM Laboratory as l WHERE l.status = :status")
+  List<Laboratory> findAllByStatus(@Param("status") Status status);
 }
